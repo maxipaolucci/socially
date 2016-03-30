460d365ad86d1ae1034b67aeb4ffa4dedf22a957
Maxi Paolucci Mac Book <mpaolucci@gmail.com>
2016-03-30 15:42:20 +1300

    added rules to the routing

--- client/routes.js
+++ client/routes.js
@@ -8,8 +8,25 @@ angular.module('socially').config(function ($urlRouterProvider, $stateProvider, 
         })
         .state('partyDetails', {
             url: '/parties/:partyId',
-            template: '<party-details></party-details>'
+            template: '<party-details></party-details>',
+            resolve: {
+                currentUser: ($q) => {
+                    if (Meteor.userId() == null) {
+                        return $q.reject();
+                    }
+                    else {
+                        return $q.resolve();
+                    }
+                }
+            }
         });
 
     $urlRouterProvider.otherwise("/parties");
+})
+.run(function ($rootScope, $state) {
+    $rootScope.$on('$stateChangeError', function (event, toState, toParams, fromState, fromParams, error) {
+        if (error === 'AUTH_REQUIRED') {
+            $state.go('parties');
+        }
+    });
 });
